ce0a58818e46b1e048ca7c3575eeea2b60f15595
Joe Davidson <79029363+jdavidson85@users.noreply.github.com>
2023-03-10 23:04:37 -0600

    Add files via upload

--- parallel_arrays_program.py
+++ parallel_arrays_program.py
@@ -1,16 +1,28 @@
-alpha = ['H', 'e', 'l', 'o', 'D', 'y', ',', ' ', '!']
-code = ['R', 'j', 'v', 'F', 'x', 'V', 'f', 'a', 'k']
+def main():
+    alpha = [' ', ',', '!', 'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i',
+             'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T',
+             't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z']
 
-encoded = []
+    code = ['!', '1', '@', '2', '#', '3', '$', '4', '%', '5', '^', '6', '&', '7', '*', '8', '(', '9', ')', '0', '_',
+            '-', '+', '=', '{', '[', '}', ']', '|', ':', ';', '<', ',', '?', '/', '~', '`', 'a', 's', 'd', 'f', 'g',
+            'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'q', 'w']
 
-text = input("Enter a phrase to encode:")
+    outline = []
 
-for x in text:
-    encoded.append(code[alpha.index(x)])
+    print("This program will take a sentence and convert it into a secret code and save it to a text file")
 
-print(encoded)
+    secret = input("Enter message you want put into code: ")
 
-f = open("encoded.txt", "w")
-for x in encoded:
-    f.write(x+"\n")
-f.close()
+    for letter in secret:
+        letter_index = alpha.index(letter)
+        code_value = code[letter_index]
+        outline.append(code_value)
+    print(outline)
+
+    outfile = open("coded.txt", "w")
+    for item in outline:
+        outfile.write(str(item) + "\n")
+    outfile.close()
+
+
+main()
